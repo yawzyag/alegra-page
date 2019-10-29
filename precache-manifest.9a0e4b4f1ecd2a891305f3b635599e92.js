b2ace9928d7e68e9523e2e303bb6f123f496d720
yawzyag <yesid.yag@gmail.com>
2019-10-28 19:34:15 -0500

    Updates

---
+++ precache-manifest.9a0e4b4f1ecd2a891305f3b635599e92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1402e155824b167c4977b55f0d7d6d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f981d716b3a422689df",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "890934fd50efb4ed4864",
+    "url": "/static/css/main.90d5cfb1.chunk.css"
+  },
+  {
+    "revision": "5f981d716b3a422689df",
+    "url": "/static/js/2.c0cd66b2.chunk.js"
+  },
+  {
+    "revision": "890934fd50efb4ed4864",
+    "url": "/static/js/main.1274c1a8.chunk.js"
+  },
+  {
+    "revision": "915831605a109f489311",
+    "url": "/static/js/runtime-main.2c513949.js"
+  },
+  {
+    "revision": "eab1abc8e05a4eafe84678a79c3154a6",
+    "url": "/static/media/logo.eab1abc8.svg"
+  }
+]);
